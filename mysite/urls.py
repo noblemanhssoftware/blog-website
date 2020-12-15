3a0dca6ad1dcfd64ba35edee66b50640396a6a45
Agi-Tuedor Noble <noblemanhs@gmail.com>
2020-12-15 23:15:58 +0100

    Initial commit

---
+++ mysite/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('secret/', admin.site.urls),
+    path('account/', include('accounts.urls', namespace='accounts')),
+    path('account/', include('django.contrib.auth.urls')),
+    path('', include('blog.urls', namespace='blog')),
+    path('services/', include('services.urls', namespace='services')),
+
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
