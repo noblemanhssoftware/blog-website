b28906a9127fe95d1e1362c589917e1d4909651e
Agi-Tuedor Noble <noblemanhs@gmail.com>
2020-12-16 08:37:11 +0100

    initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,6 +137,12 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
